4d65debec8a4704e1479cee0078dd43f51c06b1c
MalakJawabreh <145929892+MalakJawabreh@users.noreply.github.com>
2023-12-25 20:06:37 +0200

    Update MainApp.java

--- src/main/java/accessoriseSteps/MainApp.java
+++ src/main/java/accessoriseSteps/MainApp.java
@@ -3,11 +3,13 @@ package accessoriseSteps;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
 import accessoriseSteps.SignUpCustomer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainApp 
 {//
+	 private static final Logger LOGGER = Logger.getLogger(MainApp.class.getName());
 	private static SignUpCustomer cus = new SignUpCustomer();
 	private static report rep = new report();
 	private static ProfileCustomer prof=new ProfileCustomer();
@@ -33,9 +35,9 @@ public class MainApp
 
 		while (flag == 1) 
 		{
-			System.out.println("***************** Car Accessories Company ****************\n");
-			System.out.println("1. Sign Up for New Account. \n");
-			System.out.println("2. Log In \n");
+			LOGGER.log(Level.INFO,"***************** Car Accessories Company ****************\n");
+			LOGGER.log(Level.INFO,"1. Sign Up for New Account. \n");
+			LOGGER.log(Level.INFO,"2. Log In \n");
 
 			int number = Integer.parseInt(input.nextLine());
 			switch (number) {
@@ -43,26 +45,26 @@ public class MainApp
 				int flag2 = 1;
 				input1 = new Scanner(System.in);
 				while (flag2 == 1) {
-					System.out.println("1. As Admin: \n");
-					System.out.println("2. As Customer: \n");
-					System.out.println("3. As Installer: \n");
-					System.out.println("4. Close \n");
+					LOGGER.log(Level.INFO,"1. As Admin: \n");
+					LOGGER.log(Level.INFO,"2. As Customer: \n");
+					LOGGER.log(Level.INFO,"3. As Installer: \n");
+					LOGGER.log(Level.INFO,"4. Close \n");
 					int number1 = Integer.parseInt(input1.nextLine());
 					switch (number1) {
 					case 1: {
-						System.out.println("Please your Password from 3 digit only\n");
+						LOGGER.log(Level.INFO,"Please your Password from 3 digit only\n");
 						fad = 1;
 						cus.recordinformation();
 						break;
 					}
 					case 2: {
-						System.out.println("Please your Password from 4 digit only\n");
+						LOGGER.log(Level.INFO,"Please your Password from 4 digit only\n");
 						fcs = 1;
 						cus.recordinformation();
 						break;
 					}
 					case 3: {
-						System.out.println("Please your Password from 5 digit only\n");
+						LOGGER.log(Level.INFO,"Please your Password from 5 digit only\n");
 						fin = 1;
 						cus.recordinformation();
 						break;
@@ -86,9 +88,9 @@ public class MainApp
 					int flag3 = 1;
 					while (flag3 == 1)
 					{
-						System.out.println("1. Try Again: \n");
-						System.out.println("2. Sign up: \n");
-						System.out.println("3. Close \n");
+						LOGGER.log(Level.INFO,"1. Try Again: \n");
+						LOGGER.log(Level.INFO,"2. Sign up: \n");
+						LOGGER.log(Level.INFO,"3. Close \n");
 						int number3 = Integer.parseInt(input2.nextLine());
 						switch (number3) {
 						case 1: 
@@ -101,26 +103,26 @@ public class MainApp
 								int flag2 = 1;
 								input1 = new Scanner(System.in);
 								while (flag2 == 1) {
-									System.out.println("1. As Admin: \n");
-									System.out.println("2. As Customer: \n");
-									System.out.println("3. As Installer: \n");
-									System.out.println("4. Close \n");
+									LOGGER.log(Level.INFO,"1. As Admin: \n");
+									LOGGER.log(Level.INFO,"2. As Customer: \n");
+									LOGGER.log(Level.INFO,"3. As Installer: \n");
+									LOGGER.log(Level.INFO,"4. Close \n");
 									int number1 = Integer.parseInt(input1.nextLine());
 									switch (number1) {
 									case 1: {
-										System.out.println("Please your Password from 3 digit only\n");
+										LOGGER.log(Level.INFO,"Please your Password from 3 digit only\n");
 										fad = 1;
 										cus.recordinformation();
 										break;
 									}
 									case 2: {
-										System.out.println("Please your Password from 4 digit only\n");
+										LOGGER.log(Level.INFO,"Please your Password from 4 digit only\n");
 										fcs = 1;
 										cus.recordinformation();
 										break;
 									}
 									case 3: {
-										System.out.println("Please your Password from 5 digit only\n");
+										LOGGER.log(Level.INFO,"Please your Password from 5 digit only\n");
 										fin = 1;
 										cus.recordinformation();
 										break;
@@ -155,11 +157,11 @@ public class MainApp
 					input3 = new Scanner(System.in);
 					int flag4 = 1;
 					while (flag4 == 1) {
-						System.out.println("1. Products \n");
-						System.out.println("2. User Accounts \n");
-						System.out.println("3. Installation Appointment\n");
-						System.out.println("4. Generate Report \n");
-						System.out.println("5. Sign Out \n");
+						LOGGER.log(Level.INFO,"1. Products \n");
+						LOGGER.log(Level.INFO,"2. User Accounts \n");
+						LOGGER.log(Level.INFO,"3. Installation Appointment\n");
+						LOGGER.log(Level.INFO,"4. Generate Report \n");
+						LOGGER.log(Level.INFO,"5. Sign Out \n");
 						int number4 = Integer.parseInt(input3.nextLine());
 						switch (number4) {
 						case 1: {
@@ -201,16 +203,16 @@ public class MainApp
 					int flag4 = 1;
 					prof.setname(cus.getname());
 					while (flag4 == 1) {
-						System.out.println("1. Profile \n");
-						System.out.println("2. Products \n");
-						System.out.println("3. Make Order \n");
-						System.out.println("4. History \n");
-						System.out.println("5. Sign Out \n");
+						LOGGER.log(Level.INFO,"1. Profile \n");
+						LOGGER.log(Level.INFO,"2. Products \n");
+						LOGGER.log(Level.INFO,"3. Make Order \n");
+						LOGGER.log(Level.INFO,"4. History \n");
+						LOGGER.log(Level.INFO,"5. Sign Out \n");
 						int number4 = Integer.parseInt(input3.nextLine());
 						
 						switch (number4) {
 						case 1: {
-							System.out.println("\n\n If you want to edit your information choose its number :");
+							LOGGER.log(Level.INFO,"\n\n If you want to edit your information choose its number :");
 							
 							 input3 = new Scanner(System.in);
 							int flag11 = 1;
@@ -220,20 +222,20 @@ public class MainApp
 								switch (number11) {
 								case 1:
 								{
-									   System.out.println("Write the new Name :");
+									   LOGGER.log(Level.INFO,"Write the new Name :");
 									  input3 = new Scanner(System.in);
 									String name = input3.nextLine();
 									prof.changename(name); 
-									 System.out.println("sucess");
+									 LOGGER.log(Level.INFO,"sucess");
 									break;
 								}
 								case 2:
 								{
-									  System.out.println("Write the current Passward :");
+									  LOGGER.log(Level.INFO,"Write the current Passward :");
 									   input3 = new Scanner(System.in);
 									   int pass = Integer.parseInt(input3.nextLine());
 									   prof.checkpass(pass);
-									   System.out.println("Write the new Passward :");
+									   LOGGER.log(Level.INFO,"Write the new Passward :");
 									  input3 = new Scanner(System.in);
 									   int passnew = Integer.parseInt(input3.nextLine());
 									   prof.changepassward(passnew);
@@ -242,12 +244,12 @@ public class MainApp
 								case 3:
 								{
 									
-									  System.out.println("You can't edit your email.");
+									  LOGGER.log(Level.INFO,"You can't edit your email.");
 									  break;
 								}
 								case 4:
 								{
-									 System.out.println("Write the new Address :");
+									 LOGGER.log(Level.INFO,"Write the new Address :");
 									   input3 = new Scanner(System.in);
 									String add = input3.nextLine();
 									prof.changeaddress(add);   
@@ -267,11 +269,11 @@ public class MainApp
 							input3 = new Scanner(System.in);
 							int flag5 = 1;
 							while (flag5 == 1) {
-								System.out.println("1. Interior \n");
-								System.out.println("2. Exterior \n");
-								System.out.println("3. Electronics \n");
-								System.out.println("4. Search \n");
-								System.out.println("5. Back \n");
+								LOGGER.log(Level.INFO,"1. Interior \n");
+								LOGGER.log(Level.INFO,"2. Exterior \n");
+								LOGGER.log(Level.INFO,"3. Electronics \n");
+								LOGGER.log(Level.INFO,"4. Search \n");
+								LOGGER.log(Level.INFO,"5. Back \n");
 								int number5 = Integer.parseInt(input3.nextLine());
 								switch (number5) {
 								case 1: {
@@ -282,17 +284,17 @@ public class MainApp
 										pro.printint();
 										int number6 = Integer.parseInt(input3.nextLine());
 										pro.printdetails(number6);
-										System.out.println("1. Add to Order \n");
-										System.out.println("2. Back \n");
+										LOGGER.log(Level.INFO,"1. Add to Order \n");
+										LOGGER.log(Level.INFO,"2. Back \n");
 										input3 = new Scanner(System.in);
 										int number7 = Integer.parseInt(input3.nextLine());
 										switch (number7) {
 										case 1: {
-											System.out.println("Enter The Quantity: \n");
+											LOGGER.log(Level.INFO,"Enter The Quantity: \n");
 											input3 = new Scanner(System.in);
 											int number8 = Integer.parseInt(input3.nextLine());
-											System.out.println("1. Add \n");
-											System.out.println("2. Back \n");
+											LOGGER.log(Level.INFO,"1. Add \n");
+											LOGGER.log(Level.INFO,"2. Back \n");
 											input3 = new Scanner(System.in);
 											int number9 = Integer.parseInt(input3.nextLine());
 											switch (number9) {
@@ -319,8 +321,8 @@ public class MainApp
 										pro.printext();
 										int number6 = Integer.parseInt(input3.nextLine());
 										pro.printdetailsext(number6);
-										System.out.println("1. Add to Order \n");
-										System.out.println("2. Back \n");
+										LOGGER.log(Level.INFO,"1. Add to Order \n");
+										LOGGER.log(Level.INFO,"2. Back \n");
 										input3 = new Scanner(System.in);
 										int number7 = Integer.parseInt(input3.nextLine());
 										switch (number7) {
@@ -347,8 +349,8 @@ public class MainApp
 										pro.printelec();
 										int number8 = Integer.parseInt(input3.nextLine());
 										pro.printelecdetails(number8);
-										System.out.println("1. Add to Order \n");
-										System.out.println("2. Back \n");
+										LOGGER.log(Level.INFO,"1. Add to Order \n");
+										LOGGER.log(Level.INFO,"2. Back \n");
 										input3 = new Scanner(System.in);
 										int number9 = Integer.parseInt(input3.nextLine());
 
@@ -369,22 +371,22 @@ public class MainApp
 								}
 								case 4: {
 									//search
-									System.out.println("Please enter the name of the product you want to search for:");
+									LOGGER.log(Level.INFO,"Please enter the name of the product you want to search for:");
 								    String name = input3.nextLine();
 								    int number6=pro.search(name);
 									int flag6 = 1;
 									while (flag6 == 1) {
-										System.out.println("1. Add to Order \n");
-										System.out.println("2. Back \n");
+										LOGGER.log(Level.INFO,"1. Add to Order \n");
+										LOGGER.log(Level.INFO,"2. Back \n");
 										input3 = new Scanner(System.in);
 										int number7 = Integer.parseInt(input3.nextLine());
 										switch (number7) {
 										case 1: {
-											System.out.println("Enter The Quantity: \n");
+											LOGGER.log(Level.INFO,"Enter The Quantity: \n");
 											input3 = new Scanner(System.in);
 											int number8 = Integer.parseInt(input3.nextLine());
-											System.out.println("1. Add \n");
-											System.out.println("2. Back \n");
+											LOGGER.log(Level.INFO,"1. Add \n");
+											LOGGER.log(Level.INFO,"2. Back \n");
 											input3 = new Scanner(System.in);
 											int number9 = Integer.parseInt(input3.nextLine());
 											switch (number9) {
@@ -424,14 +426,14 @@ public class MainApp
 						}
 
 						case 3: {
-							System.out.println("order \n");
+							LOGGER.log(Level.INFO,"order \n");
 						
 							inst.setemail(cus.getemail2());
 							int flag7 = 1;
 							while (flag7 == 1) {
 								inst.printorder();
-								System.out.println("1. Confirm Order \n");
-								System.out.println("2. Back \n");
+								LOGGER.log(Level.INFO,"1. Confirm Order \n");
+								LOGGER.log(Level.INFO,"2. Back \n");
 								input3 = new Scanner(System.in);
 								int number9 = Integer.parseInt(input3.nextLine());
 
@@ -476,32 +478,32 @@ public class MainApp
 					input3 = new Scanner(System.in);
 					int flag4 = 1;
 					while (flag4 == 1) {
-						System.out.println("1. View installation requests \n");
-						System.out.println("2. schedule appointments \n");
-						System.out.println("3. Sign Out \n");
+						LOGGER.log(Level.INFO,"1. View installation requests \n");
+						LOGGER.log(Level.INFO,"2. schedule appointments \n");
+						LOGGER.log(Level.INFO,"3. Sign Out \n");
 						int number4 = Integer.parseInt(input3.nextLine());
 						switch (number4) {
 						case 1: 
 						{
-							System.out.println("View installation requests \n");
+							LOGGER.log(Level.INFO,"View installation requests \n");
 							inst.showinstall();
 							break;
 						}
 						case 2: 
 						{
-							System.out.println("schedule appointments \n");
+							LOGGER.log(Level.INFO,"schedule appointments \n");
 							inst.printdate();
 							input3 = new Scanner(System.in);
 							int flag9 = 1;
 							while (flag9 == 1) {
-								System.out.println("1. Add Date \n");
-								System.out.println("2. Remove Date \n");
-								System.out.println("3. Back \n");
+								LOGGER.log(Level.INFO,"1. Add Date \n");
+								LOGGER.log(Level.INFO,"2. Remove Date \n");
+								LOGGER.log(Level.INFO,"3. Back \n");
 								int number8 = Integer.parseInt(input3.nextLine());
 								switch (number8) {
 								case 1 :
 								{
-									System.out.println("/nEnter the Date :/n");
+									LOGGER.log(Level.INFO,"/nEnter the Date :/n");
 									input = new Scanner(System.in);
 									String newDate = input.nextLine();
 									inst.AddDate(newDate);
